f5bae8da4df21b60495d252bc91bdab3ec16bb13
Jason Chin <jason.rob.chin@gmail.com>
2019-01-22 14:05:11 -0500

    create template routes file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("REST API");
+/* GET example */
+router.get('/example', function(req, res, next) {
+  res.send("GET");
+});
+
+/* POST example */
+router.post('/example', function(req, res, next) {
+  res.send("POST");
+});
+
+/* PUT example */
+router.put('/example', function(req, res, next) {
+  res.send("PUT");
+});
+
+/* DELETE example */
+router.delete('/example', function(req, res, next) {
+  res.send("DELETE");
 });
 
 module.exports = router;
