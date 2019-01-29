fc2ddd9ab82a4a07a52c4e76cd918d255438e5f1
Jason Chin <jason.rob.chin@gmail.com>
2019-01-29 14:14:03 -0500

    add routes for project

--- models/user-given-roles.js
+++ models/user-given-roles.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var userGivenRolesSchema = mongoose.Schema({
   dateAssgined: Date,
   user: {type: mongoose.Schema.ObjectId, ref: 'UserProfiles'},
-  role: {type: mongoose.Schema.ObjectId, ref: 'Role'}
+  role: {type: mongoose.Schema.ObjectId, ref: 'Roles'}
 });
 
 var userGivenRoles = mongoose.model('UserGivenRoles', userGivenRolesSchema);
