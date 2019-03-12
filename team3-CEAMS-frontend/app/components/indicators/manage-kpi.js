f71d467e9b2f37e11045ff7562f501a4f5534b3f
Evan <evan.hutnik@gmail.com>
2019-03-12 19:35:40 -0400

    Peer programming forever

--- team3-CEAMS-frontend/app/components/indicators/manage-kpi.js
+++ team3-CEAMS-frontend/app/components/indicators/manage-kpi.js
@@ -1,17 +1,25 @@
 import Component from '@ember/component';
 import $ from 'jquery';
 export default Component.extend({
-    didRender() {
+  didRender() {
 
-        this._super(...arguments);
-    
-        $(document).ready(function() {
-          $('#edit-button').click(function() {
-            $('.weight').prop('readonly',false);
-          });
-          $('#save-button').click(function() {
-            $('.weight').prop('readonly',true);
-          });
-        });
-      }
+    this._super(...arguments);
+
+    $(document).ready(function () {
+      $('#staff-factors').hide();
+
+      $('#instructor-button').click(function () {
+        $('#staff-factors').hide();
+        $('#instructor-factors').show();
+        $(this).addClass('active');
+        $('#staff-button').removeClass('active');
+      });
+      $('#staff-button').click(function () {
+        $('#instructor-factors').hide();
+        $('#staff-factors').show();
+        $(this).addClass('active');
+        $('#instructor-button').removeClass('active');
+      });
+    });
+  }
 });
