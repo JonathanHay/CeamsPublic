c8e602b8ed71cb6d1e0756f9aaa926c7abc91723
jacob <jprouse2@uwo.ca>
2019-03-13 18:05:29 -0400

    commented out model method

--- team3-CEAMS-frontend/app/models/committee.js
+++ team3-CEAMS-frontend/app/models/committee.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 import { computed } from '@ember/object';
 
 export default DS.Model.extend({
-<<<<<<< HEAD
     name: DS.attr(),
     level: DS.attr(),
     dateCreated: DS.attr(),
@@ -34,37 +33,4 @@ export default DS.Model.extend({
 
     //   return info;
     // })
-=======
-  name: DS.attr(),
-  level: DS.attr(),
-  dateCreated: DS.attr(),
-  members: DS.attr(),
-
-  membersInfo: computed('members', function () {
-    let info = [];
-
-    for (let i = 0; i < this.members.length; i++) {
-      const m = this.members[i];
-
-      let memberInfo = {};
-
-      let memberType = "";
-      if (m.teachingAssistantMember != null) {
-        memberType = "teachingAssistantMember";
-      } else if (m.staffMember != null) {
-        memberType = "staffMember";
-      } else if (m.instructorMember != null) {
-        memberType = "instructorMember";
-      }
-
-      memberInfo.name = m[memberType].firstName + " " + m[memberType].lastName;
-      memberInfo.participationStartDate = new Date(m.participationStartDate).toDateString();
-      memberInfo.participationEndDate = m.participationEndDate !== null ? new Date(m.participationEndDate).toDateString() : "Still participating";
-
-      info.push(memberInfo);
-    }
-
-    return info;
-  })
->>>>>>> d080e0eeca60f50bbc44096f462b2e0c45c7166f
 });
