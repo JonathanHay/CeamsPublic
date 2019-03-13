1dca5c7a8e00f83e524b529e19d5b42417ae74c7
Evan <evan.hutnik@gmail.com>
2019-03-13 18:49:46 -0400

    Finished define evaluation method

--- team3-CEAMS-frontend/app/components/indicators/manage-kpi.js
+++ team3-CEAMS-frontend/app/components/indicators/manage-kpi.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import $ from 'jquery';
 export default Component.extend({
+  DS: service('store'),
   init() {
 
     this._super(...arguments);
@@ -26,12 +28,12 @@ export default Component.extend({
     submitInstructorKPI(e) {
       e.preventDefault();
       let weights = {
-        logins: this.get('instructorLogins'),
-        testsGraded: this.get('instructorTestsGraded'),
-        classesTaught: this.get('instructorClassesTaught'),
-        actions: this.get('instructorActions'),
+        numLogins: this.get('instructorLogins'),
+        numGraded: this.get('instructorTestsGraded'),
+        numCourses: this.get('instructorClassesTaught'),
+        totalActions: this.get('instructorActions'),
       };
-      
+
       let sum = 0;
       for (const key in weights) {
         if (weights.hasOwnProperty(key)) {
@@ -43,9 +45,64 @@ export default Component.extend({
 
       if (sum == 100) {
         console.log('submit');
+
+        console.log(weights);
+
+        var submission = {
+          formulaType: "Instructor",
+          formulaExpression: JSON.stringify(weights),
+          formulaDescription: this.get('instructorMethodDescription') || "",
+          staff: [],
+          instructors: []
+        }
+
+        console.log(submission);
+
+        let newUEM = this.DS.createRecord('user-evaluation-method', submission);
+        newUEM.save().then(() => { alert('we did it') }, () => { alert('we failed') })
+
         $('#instructor-factors .error-message').hide();
       } else {
         $('#instructor-factors .error-message').show();
+      }
+    },
+    submitStaffKPI(e) {
+      let weights = {
+        numLogins: this.get('staffLogins'),
+        totalActions: this.get('staffActions'),
+      };
+
+      let sum = 0;
+      for (const key in weights) {
+        if (weights.hasOwnProperty(key)) {
+          const w = weights[key];
+          if (w === undefined) sum += 0;
+          else sum += parseInt(w);
+        }
+      }
+
+      if (sum == 100) {
+        console.log('submit');
+
+        var submission = {
+          formulaType: "Staff",
+          formulaExpression: JSON.stringify(weights),
+          formulaDescription: this.get('staffMethodDescription') || "",
+          staff: [],
+          instructors: []
+        }
+
+        console.log(submission);
+
+        let newUEM = this.DS.createRecord('user-evaluation-method', submission);
+        newUEM.save().then(() => { alert('we did it') }, () => { alert('we failed') })
+
+
+
+
+        $('#staff-factors .error-message').hide();
+      } else {
+        $('#staff-factors .error-message').show();
       }
     }
   }
