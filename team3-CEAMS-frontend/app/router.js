d514c682257c4b7e991b635713bea7a01a02e5db
JonathanHay <jonathanhay0938@gmail.com>
2019-03-25 14:38:46 -0400

    Probably fixed routes

--- team3-CEAMS-frontend/app/router.js
+++ team3-CEAMS-frontend/app/router.js
@@ -6,37 +6,37 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('home-page');
+Router.map(function () {
+  this.route('home-page', { path: '/' });
   this.route('login');
   this.route('datatable-example');
-  this.route('ceams', function() {
+  this.route('ceams', function () {
     this.route('about');
     this.route('manage-user-accounts');
     this.route('manage-roles');
     this.route('manage-capabilities');
     this.route('courses');
 
-    this.route('committees', function() {
-      this.route('detail');
-      this.route('manage-users.js');
+    this.route('committees', function () {
+      this.route('detail', { path: '/detail/:committee_id' });
+      this.route('manage-users.js', { path: '//:committee_id' });
       this.route('team-detail');
     });
 
-    this.route('indicators', function() {
+    this.route('indicators', function () {
       this.route('list-kpi');
       this.route('manage-upi');
       this.route('manage-kpi');
       this.route('assign-evaluation-methods');
     });
-    
+
     this.route('new-task-force');
     this.route('meeting');
     this.route('ga-task-force');
     this.route('delete-task-force');
   });
 
-  this.route('seams', function() {});
+  this.route('seams', function () { });
 });
 
 export default Router;
