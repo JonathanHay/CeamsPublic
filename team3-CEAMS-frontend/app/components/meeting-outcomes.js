092fe5e7352b2264ffd857b42a9c0c36a0662ee7
jacob <jprouse2@uwo.ca>
2019-03-26 15:20:26 -0400

    added more permissions

--- team3-CEAMS-frontend/app/components/meeting-outcomes.js
+++ team3-CEAMS-frontend/app/components/meeting-outcomes.js
@@ -6,6 +6,46 @@ export default Component.extend({
     didRender() {
         this._super(...arguments);
     },
+    FEAT022_000IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_000") >= 0);
+        }
+    }),
+    FEAT022_001IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_001") >= 0);
+        }
+    }),
+    FEAT022_002IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_002") >= 0);
+        }
+    }),
+    FEAT022_003IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_003") >= 0);
+        }
+    }),
+    FEAT022_004IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_004") >= 0);
+        }
+    }),
     DS: service('store'),
     actions: {
         addToTable: function () {
