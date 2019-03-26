3015572d43c39510b2755c4db456c909be792f0d
jacob <jprouse2@uwo.ca>
2019-03-26 14:32:44 -0400

    permissions on meetings

--- team3-CEAMS-frontend/app/components/meeting-component.js
+++ team3-CEAMS-frontend/app/components/meeting-component.js
@@ -1,8 +1,51 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import { inject as service } from '@ember/service';
+
 
 export default Component.extend({
     didInsertElement() {
         this._super(...arguments);
         $('#example').DataTable();
-    }
+    },
+    FEAT022_000IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_000") >= 0);
+        }
+    }),
+    FEAT022_001IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_001") >= 0);
+        }
+    }),
+    FEAT022_002IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_002") >= 0);
+        }
+    }),
+    FEAT022_003IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_003") >= 0);
+        }
+    }),
+    FEAT022_004IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_004") >= 0);
+        }
+    }),
 });
--- team3-CEAMS-frontend/app/components/view-meeting.js
+++ team3-CEAMS-frontend/app/components/view-meeting.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import { oneWay } from '@ember/object/computed';
 import $ from 'jquery';
 
 export default Component.extend({
@@ -17,6 +16,47 @@ export default Component.extend({
 
     modalName: computed(function () {
         return 'newMeeting' + this.get('ID');
+    }),
+
+    FEAT022_000IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_000") >= 0);
+        }
+    }),
+    FEAT022_001IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_001") >= 0);
+        }
+    }),
+    FEAT022_002IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_002") >= 0);
+        }
+    }),
+    FEAT022_003IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_003") >= 0);
+        }
+    }),
+    FEAT022_004IsPermitted: computed(function(){ //Delete course
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT022_004") >= 0);
+        }
     }),
 
     actions: {
