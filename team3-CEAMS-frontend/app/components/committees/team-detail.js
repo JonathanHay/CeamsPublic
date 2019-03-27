a01273e6efeef21e58a6631e91d497bd8ec5a36b
swei57 <swei57@users.noreply.github.com>
2019-03-26 21:29:30 -0400

    added feat21_000 to stop unauthenticated users from managing users in a GA task force

--- team3-CEAMS-frontend/app/components/committees/team-detail.js
+++ team3-CEAMS-frontend/app/components/committees/team-detail.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import { inject as service } from '@ember/service';
+
 
 export default Component.extend({
+
+    FEAT021_000IsPermitted: computed(function(){ //manage ga task force or ga committee
+        var authentication = this.get('oudaAuth');
+        if (authentication.getName === "Root") {
+          return true;
+        } else {
+          return (authentication.get('userCList').indexOf("FEAT021_000") >= 0);
+        }
+    }),
 });
