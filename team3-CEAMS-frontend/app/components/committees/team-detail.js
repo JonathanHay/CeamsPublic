d692b0c80f1353b00ffc503fabe87c69d7830a4c
JonathanHay <jonathanhay0938@gmail.com>
2019-03-26 22:08:52 -0400

    fixed feature permissions for team-detail->manage-users

--- team3-CEAMS-frontend/app/components/committees/team-detail.js
+++ team3-CEAMS-frontend/app/components/committees/team-detail.js
@@ -1,16 +1,16 @@
 import Component from '@ember/component';
-import {computed} from '@ember/object';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 
 export default Component.extend({
 
-    FEAT021_000IsPermitted: computed(function(){ //manage ga task force or ga committee
-        var authentication = this.get('oudaAuth');
-        if (authentication.getName === "Root") {
-          return true;
-        } else {
-          return (authentication.get('userCList').indexOf("FEAT021_000") >= 0);
-        }
-    }),
+  FEAT21_002IsPermitted: computed(function () { //manage ga task force or ga committee
+    var authentication = this.get('oudaAuth');
+    if (authentication.getName === "Root") {
+      return true;
+    } else {
+      return (authentication.get('userCList').indexOf("FEAT21_002") >= 0);
+    }
+  }),
 });
