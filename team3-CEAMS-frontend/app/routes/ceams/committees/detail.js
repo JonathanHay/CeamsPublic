8aad034be4c3374a11a71a37522ffb7912d89013
Jason Chin <jason.rob.chin@gmail.com>
2019-04-02 12:50:07 -0400

    fix detail page

--- team3-CEAMS-frontend/app/routes/ceams/committees/detail.js
+++ team3-CEAMS-frontend/app/routes/ceams/committees/detail.js
@@ -6,18 +6,26 @@ export default Route.extend({
     let members = await committee.get('members');
     let membersInfo = [];
 
+    console.time('start');
     for (let i = 0; i < members.length; i++) {
       const m = members.objectAt(i);
       
       let info = await m.get('teachingAssistantMember') 
-              || await m.get('instructorMember') 
-              ||  await m.get('staffMember');
-      membersInfo.push({
-        name: info.firstName + " " + info.lastName,
-        startDate: m.participationStartDate,
-        endDate: m.participationEndDate || "Still participating"
-      });
+      || await m.get('instructorMember') 
+      ||  await m.get('staffMember');
+      
+      try {
+        membersInfo.push({
+          name: info.firstName + " " + info.lastName,
+          startDate: m.participationStartDate,
+          endDate: m.participationEndDate || "Still participating"
+        });
+        
+      } catch (error) {
+        
+      }
     }
+    console.timeEnd('start');
 
     return {
       committee,
